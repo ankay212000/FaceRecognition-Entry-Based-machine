b8abb0c09f2cad52ed7e867ddeb9267e6f4ddeb7
ANKAY <52236029+ankay212000@users.noreply.github.com>
2021-03-16 10:15:49 +0530

    Delete Initialise_firebase.py

--- Initialise_firebase.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials,storage
-from google.cloud import firestore
-from firebase_admin import firestore
-
-def initialise():
-    cred = credentials.Certificate(r".\jsonKeys\ServiceAccountKey.json")
-    firebase_admin.initialize_app(cred, {"databaseURL": "https://face.firebaseio.com/",'storageBucket': 'face-b84fc.appspot.com'})
